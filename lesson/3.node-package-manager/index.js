ee3cfb129ac1b3baefb0405a27486a129a9a6059
meshackkirwachumba <meshackkirwachumba@gmail.com>
2026-02-14 11:16:26 +0300

    node package manager

---
+++ lesson/3.node-package-manager/index.js
@@ -0,0 +1,9 @@
+const lodash = require("lodash");
+
+console.log(lodash.add(1, 2));
+
+names = ["Alice", "Bob", "Charlie"];
+
+console.log(lodash.shuffle(names));
+
+console.log(lodash.random(1, 100));
